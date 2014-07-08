315853363eb214ee84fc4d688743539691bc3e56
Maurice van Creij <maurice.vancreij@webqem.com>
2014-07-08 14:14:03 +1000

    Reduced reliance on typography.

--- inc/js/slideshow.js
+++
@@ -1,7 +0,0 @@
-/*
-	Combined JavaScripts
-
-	@codekit-append "../src/useful.slideshow.js"
-	@codekit-append "../../useful-polyfills/src/useful.polyfills.js"
-	@codekit-append "../../useful-transitions/src/useful.transitions.js"
-*/
